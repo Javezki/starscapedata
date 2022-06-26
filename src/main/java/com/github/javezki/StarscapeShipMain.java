0aa512bc052ea8fd4a0248f3922456bbecd5dccf
BuildTools <unconfigured@null.spigotmc.org>
2022-06-26 17:18:20 -0400

    initial

---
+++ src/main/java/com/github/javezki/StarscapeShipMain.java
@@ -0,0 +1,16 @@
+package com.github.javezki;
+
+/*
+ * Created: 2022-06-26
+ * Author: Javezki
+ * Purpose: A program to display all data of ships with certain modules from starscape
+ * Special Mention: Taemonster06
+ */
+
+public class StarscapeShipMain 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
